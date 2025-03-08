a431f3531a658e5f18a03c9d1149437bb67da1a0
r1h1 <de.rivasherrera@gmail.com>
2025-03-07 22:52:41 -0600

    Se realizan cambios en los IDs, datatables y diseño en general

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,23 +1,16 @@
-document.addEventListener("DOMContentLoaded", function () {
-
+const responsiveMenu = function () {
     const menuBtn = document.getElementById("menuToggle");
     const sidebar = document.getElementById("sidebar");
     const content = document.getElementById("content");
 
-    if (!menuBtn || !sidebar || !content) {
-        return;
+    // Verifica si el sidebar está oculto
+    if (sidebar.classList.contains("hidden")) {
+        sidebar.style.transform = "translateX(0)"; // Muestra el sidebar
+        sidebar.classList.remove("hidden");
+        content.classList.remove("shift");
+    } else {
+        sidebar.style.transform = "translateX(-100%)"; // Oculta el sidebar
+        sidebar.classList.add("hidden");
+        content.classList.add("shift");
     }
-
-    menuBtn.addEventListener("click", function () {
-        // Verifica si el sidebar está oculto
-        if (sidebar.classList.contains("hidden")) {
-            sidebar.style.transform = "translateX(0)"; // Muestra el sidebar
-            sidebar.classList.remove("hidden");
-            content.classList.remove("shift");
-        } else {
-            sidebar.style.transform = "translateX(-100%)"; // Oculta el sidebar
-            sidebar.classList.add("hidden");
-            content.classList.add("shift");
-        }
-    });
-});
+}
